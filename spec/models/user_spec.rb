2ff0fdf4115b8b6f227a4ef103db3d21b7e2cca9
kaydc18 <klattk@gmail.com>
2020-10-20 13:22:46 -0400

    added devise functionality and testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "adding a user with all fields filled" do
+    it "should save the user" do
+      user = FactoryBot.create(:user)
+      expect(user.save).to eq(true)
+    end
+  end
+
+  describe "adding a user with missing field" do
+    it "should not save the user" do
+      user = FactoryBot.create(:user)
+      user.first_name = ""
+      expect(user.save).to eq(false)
+    end
+  end
 end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -2,6 +2,8 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :user do
+    sequence(:first_name) {|n| "first name #{n}" }
+    sequence(:last_name) {|n| "last name #{n}" }
     sequence(:email) {|n| "user#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
