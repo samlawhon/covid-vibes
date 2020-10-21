e50acc8edeb4ff23a2a4a122e62f52f4d088f4fb
Daniel  Shnaider <danielshnaider@MacBook-Pro.local>
2020-10-21 13:22:22 -0400

    front-end-50-percent

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,30 +1,42 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import RestaurantIndex from "./RestaurantIndex.js"
+// import restaurantsdata from "/constants/restaurantsdata"
 
 
-
-export const App = () => {
-
-  useEffect(()=>{
-    fetch('./restaurants.json')
-    .then(response => response.json())
-    .then(responseBody => {
- 
-    })
-  })
-
-  const restaurantName = "Big Harry's"
-  const restaurantDesc = "The best barbecue"
-  const wearMasks = false
-  const socialDistance = false
+const App = () => {
+  const restaurantData = [
+    {
+      "id": 500678894,
+      "latitude": 42.379816,
+      "longitude": 42.379816,
+      "name": "El Potro"
+    },
+    {
+      "id": 500792317,
+      "latitude": 42.3734909,
+      "longitude": 42.3734909,
+      "name": "All-Star Sandwich Bar"
+    },
+    {
+      "id": 500792356,
+      "latitude": 42.3734808,
+      "longitude": 42.3734808,
+      "name": "S&S Restaurant"
+    }]
+  const data = restaurantData
+  // const restaurantName = "Big Harry's"
+  // const restaurantDesc = "The best barbecue"
+  // const wearMasks = false
+  // const socialDistance = false
 
   return (<div>
   <h1>Watson, Come here, I need you!</h1>
     <RestaurantIndex
-      name={restaurantName}
-      description={restaurantDesc}
-      masks={wearMasks}
-      socialDistance={socialDistance}
+      // name={restaurantName}
+      // description={restaurantDesc}
+      // masks={wearMasks}
+      // socialDistance={socialDistance}
+      data={data.}
     />
   </div>)
 }
---
+++ app/javascript/react/components/RestaurantShow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const RestaurantShow = (props) => {
+    const restaurantName = "Big Harry's"
+    const restaurantDesc = "The best barbecue"
+    const wearMasks = false
+    const socialDistance = false
+
+    return (<h1>Test</h1>)
+}
+
+export default RestaurantShow
