eac3d3a1378217f0923f2980402d33b827a3ff80
Salim Akram <salimakram123@gmail.com>
2020-10-22 12:11:53 -0700

    just commiting to get back to a different bran

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,6 @@ ActiveRecord::Schema.define(version: 2020_10_20_203844) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
