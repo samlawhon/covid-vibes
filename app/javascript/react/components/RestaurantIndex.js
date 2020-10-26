25f0997b25fcd55a1789f342dd4226000af4d387
Salim Akram <salimakram123@gmail.com>
2020-10-26 10:35:00 -0400

    fixed tab spacing

--- app/javascript/react/components/RestaurantIndex.js
+++ app/javascript/react/components/RestaurantIndex.js
@@ -6,31 +6,31 @@ const RestaurantIndex = (props) => {
   const [restaurantData, setRestaurantData] = useState([])
 
    useEffect(() => {
-       fetch('/api/v1/restaurants?location=boston')
-             .then(response => response.json())
-             .then(responseBody => {
-                 setRestaurantData(responseBody)
-             })
-      }, [])
+    fetch('/api/v1/restaurants?location=boston')
+      .then(response => response.json())
+      .then(responseBody => {
+          setRestaurantData(responseBody)
+      })
+    }, [])
 
   const restaurantTileArray  = restaurantData.map((restaurant) => {
       
-        return(
-            <div>
-                <RestaurantTile
-                    name={restaurant.name}
-                    id={restaurant.id}
-                    key={restaurant.id}
-                />
-            </div>
-        )
-    }) 
-
     return(
-        <div>
-            {restaurantTileArray}
-        </div>
+      <div>
+        <RestaurantTile
+          name={restaurant.name}
+          id={restaurant.id}
+          key={restaurant.id}
+        />
+      </div>
     )
+  }) 
+
+  return(
+    <div>
+      {restaurantTileArray}
+    </div>
+  )
 }  
   
 export default RestaurantIndex      
--- app/javascript/react/components/RestaurantTile.js
+++ app/javascript/react/components/RestaurantTile.js
@@ -4,13 +4,11 @@ import { Link } from "react-router-dom"
 const RestaurantTile = (props) => {
 
   return (
-    <div>
       <h1>
         <Link to={`restaurant/${props.id}`}>
           {props.name}
         </Link > 
       </h1>
-    </div>
   )
 }
 
