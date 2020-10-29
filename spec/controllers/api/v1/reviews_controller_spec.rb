09b20fb7d1e263721e4f06e94d823cca175c7198
Salim Akram <salimakram123@gmail.com>
2020-10-29 13:40:16 -0400

    stashed stuffed before switch

---
+++ spec/controllers/api/v1/reviews_controller_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+require "json"
+
+RSpec.describe Api::V1::ReviewsController, type: :controller do
+  describe "POST#create" do
+    context "when a request correct params is made" do
+
+
+    end
+  end
+end
+
+
