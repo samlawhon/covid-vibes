3b0888efe9018df81d4260d8f17729f9bdf50198
Salim Akram <salimakram123@gmail.com>
2020-10-30 10:25:19 -0400

    search bar functionality

--- app/javascript/react/components/RestaurantIndex.js
+++ app/javascript/react/components/RestaurantIndex.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, Fragment } from "react";
 import RestaurantTile from "./RestaurantTile";
 import RestaurantMap from "./RestaurantsMap";
+import SearchForm from "./SearchForm";
 
 const RestaurantIndex = () => {
 
@@ -24,6 +25,25 @@ const RestaurantIndex = () => {
     .catch(error => console.error(`Error in fetch: ${error.message}`))
   }, []);
 
+  const handleSubmit = (event, searchBarQuery) => {
+    event.preventDefault();
+    fetch(`/api/v1/restaurants?location=${searchBarQuery.trim()}`)
+      .then(response => {
+        if (response.ok) {
+          return response
+        } else {
+          const errorMessage = `${response.status} (${response.statusText})`;
+          const error = new Error(errorMessage);
+          throw (error);
+        }
+      })
+      .then(response => response.json())
+      .then(responseBody => {
+        setRestaurantData(responseBody)
+      })
+      .catch(error => console.error(`Error in fetch: ${error.message}`))
+  }
+
   const restaurantTileArray = restaurantData.map((restaurant) => (
     <RestaurantTile
       name={restaurant.name}
@@ -41,23 +61,12 @@ const RestaurantIndex = () => {
           <img src="/assets/CovidVibesLogo2.png"/>
         </div>
       </div>
-      <div className="square-box grid-y medium-grid-frame grid-padding-y .grid-margin-y">
-        <form>
-          <div className="grid-x align-right align-bottom grid-padding-x">
-              <div className="medium-7 cell">
-                  <label>Zipcode
-                    <input type="text" placeholder="zipcode" />
-                  </label>
-              </div>
-              <div className="medium-3 cell">
-                <input type="submit" className="button" value="Submit" />
-              </div>
-          </div>
-        </form>
+      <div className="square-box grid-y medium-grid-frame grid-padding-y .grid-margin-y">    
+        <SearchForm handleSubmit={handleSubmit}/>
         <div className="cell medium-auto medium-cell-block-container">
           <div className="grid-x grid-padding-x" >
             <div className="cell medium-6 medium-cell-block-y">
-                {restaurantTileArray}
+              {restaurantTileArray}
             </div>
             <div className="cell medium-6 medium-cell-block-y">
               {restaurantMap}
